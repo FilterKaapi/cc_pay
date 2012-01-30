d7582cb970502f6069567f008edbd42081dad04b
Sandeep Laxman <sandeep.laxman@gmail.com>
2012-01-30 11:53:51 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CcPay::Application.config.secret_token = '63a1cea11652d971d8fc2b6caa03845c2cdfcd19c39a3e056ce737cefdb6a0b1a150682b2f0a3b119983d1ae4efec78f0185e80af4b400ac57c3df989f593070'
